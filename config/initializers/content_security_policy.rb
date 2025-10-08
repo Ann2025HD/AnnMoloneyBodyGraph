6ae3dfc2c43e7e8b5ef18f333bd59c70112341ee
Ann Moloney <you@example.com>
2025-10-08 22:32:14 +0100

    chore: CSP for Google Maps/Places + allow WP iframe

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,9 +1,23 @@
+# config/initializers/content_security_policy.rb
 if Rails.env.production?
-  Rails.application.config.content_security_policy do |policy|
-    # Allow your WP site(s) to embed your app in an iframe
-    policy.frame_ancestors :self,
-      "https://annmoloney.com",
-      "https://www.annmoloney.com"
+  Rails.application.config.content_security_policy do |p|
+    p.default_src :self
+
+    # Allow Google Maps/Places JS + polyfills
+    p.script_src  :self, "https://maps.googleapis.com", "https://maps.gstatic.com", "https://polyfill.io", :unsafe_inline
+
+    # XHR/fetch used by Places
+    p.connect_src :self, "https://maps.googleapis.com"
+
+    # Fonts & styles Google uses
+    p.style_src   :self, "https://fonts.googleapis.com", :unsafe_inline
+    p.font_src    :self, "https://fonts.gstatic.com", :data
+
+    # Images/icons from Google + data URIs
+    p.img_src     :self, :data, "https://maps.gstatic.com", "https://maps.googleapis.com"
+
+    # Let your WP site embed the app (iframe)
+    p.frame_ancestors "https://annmoloney.com", "https://www.annmoloney.com"
   end
 
   # Remove the legacy header so CSP controls framing
