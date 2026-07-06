79687dc26c7e526621aeb0681eeb189f4efc9246
Ann Moloney <you@example.com>
2026-07-06 22:29:24 +0100

    Simplify CSP for MailerLite embed

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,6 +1,8 @@
 # config/initializers/content_security_policy.rb
 
 # --- Production CSP (enforced) ---
+
+
 if Rails.env.production?
   Rails.application.config.content_security_policy do |p|
     p.default_src :self
@@ -17,10 +19,6 @@ if Rails.env.production?
     # Allow your WordPress site to embed this app (iframe)
     p.frame_ancestors "https://annmoloney.com", "https://www.annmoloney.com"
   end
-
-  # Nonces for inline/module scripts (if you use them in prod)
-  Rails.application.config.content_security_policy_nonce_generator =
-    ->(_req) { SecureRandom.base64(16) }
 
   # Let CSP (not X-Frame-Options) control framing
   Rails.application.config.action_dispatch.default_headers.delete("X-Frame-Options")
