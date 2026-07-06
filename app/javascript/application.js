d0717a0faf18c828ed73aba75daaaf4c4a538627
Ann Moloney <you@example.com>
2026-07-06 23:42:45 +0100

    Replace MailerLite embed with HTML form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,15 +2,3 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-(function(w,d,e,u,f,l,n){
-  w[f]=w[f]||function(){
-    (w[f].q=w[f].q||[]).push(arguments);
-  };
-  l=d.createElement(e);
-  l.async=1;
-  l.src=u;
-  n=d.getElementsByTagName(e)[0];
-  n.parentNode.insertBefore(l,n);
-})(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-
-ml('account', '1270515');
